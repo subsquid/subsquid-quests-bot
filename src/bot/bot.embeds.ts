d9bacb49e5790ecea8b32a757fbd349f80941865
Eugene Formanenko <mo4islona@gmail.com>
2022-03-09 12:09:00 +0300

    update message title

--- src/bot/bot.embeds.ts
+++ src/bot/bot.embeds.ts
@@ -7,9 +7,9 @@ export class BotEmbeds {
 
     prepareQuestAnnounce(quest: Quest): MessageOptions {
         const actions = this.createActions(quest);
-        const message = { 
-          content: 'New quest available', 
-          embeds: [this.createAnnouncementEmbed(quest)] 
+        const message = {
+          content: 'New quest available',
+          embeds: [this.createAnnouncementEmbed(quest)]
         } as MessageOptions;
         if(actions) {
           message.components = [actions];
@@ -23,23 +23,23 @@ export class BotEmbeds {
         const claimButton = new MessageButton().setCustomId(`claim_${quest.id}`).setLabel('Claim').setStyle('PRIMARY')
         const unClaimButton = new MessageButton().setCustomId(`unclaim_${quest.id}`).setLabel('Unclaim').setStyle('PRIMARY');
         const submitForReviewButton = new MessageButton().setCustomId(`submit_${quest.id}`).setLabel('Submit for Review').setStyle('PRIMARY');
-    
+
         const unclaimButtonNeeded = quest.applicants?.length >=1;
         switch(quest.status) {
-          case 'OPEN': 
+          case 'OPEN':
             return new MessageActionRow().addComponents(unclaimButtonNeeded ? [claimButton, unClaimButton]: [claimButton]);
             break;
           case 'CLAIMED':
             return new MessageActionRow().addComponents(unclaimButtonNeeded ? [unClaimButton, submitForReviewButton]: [submitForReviewButton]);
             break;
-          default: 
+          default:
             return null as unknown as MessageActionRow;
         }
       }
-    
+
       createAnnouncementEmbed(q: Quest): MessageEmbed {
         const announcementEmbed = new MessageEmbed()
-          .setTitle(`🏛 🏛 🏛 ${q.title}`)
+          .setTitle(`🦑 ${q.title} 🦑`)
           .setDescription(`${q.description}`)
           .addField('ID', `${q.id}`, true)
           .addField('Valid until', `${q.expiresOn}`, true)
@@ -47,7 +47,7 @@ export class BotEmbeds {
           .addField('Reward', `${q.rewards}`, true)
           .addField('Status', `${q.status}`, true)
           .setTimestamp();
-    
+
         if(q.applicants?.length as number > 0) {
           announcementEmbed.addField('Applicants', `${q.applicants?.map((applicant) => 
             `📁 **${applicant.get().discordHandle}**`).join('\n')}`)
